0cd658d5471d2aacf0c307a5c3903a1ad40864f0
Hugo Lispector <hugo_lispector@apple.com>
2022-03-30 21:38:23 -0300

    Finished bridge supports view.

--- BridgeBuilder/User Interface/ContentView.swift
+++ BridgeBuilder/User Interface/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
     @State var stressLimit = "300e6"
     @State var floorSupportsX = "-30, 30"
     @State var extraSupportsX = "0"
-    @State var extraSupportsY = "-10"
+    @State var extraSupportsY = "-20"
 
     var floorSupports: [CGPoint] {
         let floorXPositionsStrings = floorSupportsX.components(separatedBy: ", ")
@@ -57,82 +57,112 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-            Form {
-                Text("Edges")
-                    .font(.body.bold())
-                Section() {
-                    TextField(text: $maxEdgeLength, prompt: Text("Ex: 15")) {
-                        Text("Maximum length:")
+                Form {
+                    Text("Edges")
+                        .font(.body.bold())
+                    Section() {
+                        TextField(text: $maxEdgeLength, prompt: Text("Ex: 15")) {
+                            Text("Maximum length:")
+                        }
+                        TextField(text: $edgeElasticity, prompt: Text("Ex: 210e9")) {
+                            Text("Elasticity:")
+                        }
+                        TextField(text: $edgeDensity, prompt: Text("Ex: 7850")) {
+                            Text("Density:")
+                        }
                     }
-                    TextField(text: $edgeElasticity, prompt: Text("Ex: 210e9")) {
-                        Text("Elasticity:")
+                    Text("Vertices")
+                        .font(.body.bold())
+                        .padding(.top, 12)
+                    Section() {
+                        TextField(text: $maxEdgeLength, prompt: Text("Ex: 3")) {
+                            Text("Minimum distance:")
+                        }
                     }
-                    TextField(text: $edgeDensity, prompt: Text("Ex: 7850")) {
-                        Text("Density:")
+                    Text("Evolution Chamber")
+                        .font(.body.bold())
+                        .padding(.top, 12)
+                    Section() {
+                        TextField(text: $edgeElasticity, prompt: Text("Ex: 100")) {
+                            Text("Elasticity:")
+                        }
+                        TextField(text: $generationLimit, prompt: Text("Ex: 300")) {
+                            Text("Generation limit:")
+                        }
+                        TextField(text: $replacementRate, prompt: Text("Ex: 0.3")) {
+                            Text("Replacement rate:")
+                        }
+                        TextField(text: $crossoverRadius, prompt: Text("Ex: 30")) {
+                            Text("Crossover radius:")
+                        }
                     }
-                }
-                Text("Vertices")
-                    .font(.body.bold())
-                    .padding(.top, 12)
-                Section() {
-                    TextField(text: $maxEdgeLength, prompt: Text("Ex: 3")) {
-                        Text("Minimum distance:")
+                    Text("Bridge")
+                        .font(.body.bold())
+                        .padding(.top, 12)
+                    Section() {
+                        TextField(text: $load, prompt: Text("Ex: 2e6")) {
+                            Text("Load:")
+                        }
+                        TextField(text: $stressLimit, prompt: Text("Ex: 300e6")) {
+                            Text("Stress limit:")
+                        }
+                        TextField(text: $floorSupportsX, prompt: Text("Ex: -30, 30")) {
+                            Text("Floor supports X's:")
+                        }
+                        TextField(text: $extraSupportsX, prompt: Text("Ex: 0")) {
+                            Text("Extra supports X's:")
+                        }
+                        TextField(text: $extraSupportsY, prompt: Text("Ex: -20")) {
+                            Text("Extra supports Y's:")
+                        }
                     }
-                }
-                Text("Evolution Chamber")
-                    .font(.body.bold())
-                    .padding(.top, 12)
-                Section() {
-                    TextField(text: $edgeElasticity, prompt: Text("Ex: 100")) {
-                        Text("Elasticity:")
-                    }
-                    TextField(text: $generationLimit, prompt: Text("Ex: 300")) {
-                        Text("Generation limit:")
-                    }
-                    TextField(text: $replacementRate, prompt: Text("Ex: 0.3")) {
-                        Text("Replacement rate:")
-                    }
-                    TextField(text: $crossoverRadius, prompt: Text("Ex: 30")) {
-                        Text("Crossover radius:")
-                    }
-                }
-                Text("Bridge")
-                    .font(.body.bold())
-                    .padding(.top, 12)
-                Section() {
-                    TextField(text: $load, prompt: Text("Ex: 2e6")) {
-                        Text("Load:")
-                    }
-                    TextField(text: $stressLimit, prompt: Text("Ex: 300e6")) {
-                        Text("Stress limit:")
-                    }
-                    TextField(text: $floorSupportsX, prompt: Text("Ex: -30, 30")) {
-                        Text("Floor supports X's:")
-                    }
-                    TextField(text: $extraSupportsX, prompt: Text("Ex: 20")) {
-                        Text("Extra supports X's:")
-                    }
-                    TextField(text: $extraSupportsY, prompt: Text("Ex: -20")) {
-                        Text("Extra supports Y's:")
-                    }
-                }
-                Spacer()
-            }
-            }
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Button {
-                        startEvolutionProcess()
-                    } label: {
-                        Text("Run \(Image(systemName: "play.fill"))")
-                    }
+                    Spacer()
                 }
             }
             .padding()
             .frame(minWidth: 300)
 
-            Text("Detail View")
+            let scaler: CGFloat = 4
+            ZStack {
+                ForEach(Array(zip(floorSupports, floorSupports.indices)), id: \.1) { point, index in
+                    ZStack {
+                        Image(systemName: "arrowtriangle.up.fill")
+                            .foregroundColor(.green)
+                            .font(.title)
+                            .offset(x: point.x * scaler, y: +9 - point.y * scaler)
+                        if index == 0 {
+                            Image(systemName: "arrowtriangle.right.fill")
+                                .foregroundColor(.green)
+                                .font(.title)
+                                .offset(x: -9 + point.x * scaler, y: -point.y * scaler)
+                        }
+                    }
+                }
+                let leftFloor = floorSupports.map{ $0.x }.min() ?? 0
+                let rightFloor = floorSupports.map{ $0.x }.max() ?? 0
+                Rectangle()
+                    .foregroundColor(.blue)
+                    .frame(width: (rightFloor-leftFloor) * scaler, height: 2)
+                    .offset(x: (leftFloor + rightFloor)/2 * scaler, y: -floorSupports[0].y * scaler)
+
+                ForEach(Array(zip(extraSupports, extraSupports.indices)), id: \.1) { point, index in
+                    Image(systemName: "arrowtriangle.up.fill")
+                        .foregroundColor(.green)
+                        .font(.title)
+                        .offset(x: point.x * scaler, y: +9 - point.y * scaler)
+                }
+            }
+            .offset(y: -15 * scaler)
                 .navigationTitle("Bridge Builder")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button {
+                            startEvolutionProcess()
+                        } label: {
+                            Text("Run \(Image(systemName: "play.fill"))")
+                        }
+                    }
+                }
         }
     }
 
